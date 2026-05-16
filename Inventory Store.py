f037e703b9d4ce501d72639901a7e9b5ade994f8
Sandeep K S <61325558+sandeepks011999@users.noreply.github.com>
2026-05-16 09:41:26 -0700

    Inventory Store
    Initialize inventory with mobile phone and Laptop details.

---
+++ Inventory Store.py
@@ -0,0 +1,20 @@
+inventory={}
+product_name="mobile phone"
+product_quantity=5
+product_price=20000
+product_release_year=2020
+inventory["product_name"]=product_name
+inventory["product_quantity"]=product_quantity
+inventory["product_price"]=product_price
+inventory["product_release_year"]=product_release_year
+product_name1="Laptop"
+product_quantity1=10
+product_price1=50000
+product_release_year1=2023
+inventory["product_name1"]=product_name1
+inventory["product_quantity1"]=product_quantity1
+inventory["product_price1"]=product_price1
+inventory["product_release_year1"]=product_release_year1
+print(inventory)
+print("product_release_year" in inventory)
+print("product_release_year1" in inventory)
