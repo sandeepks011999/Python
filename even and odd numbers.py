0f2c4d29efcd85a02b416f58124fbe0e3f5a5b76
Sandeep K S <61325558+sandeepks011999@users.noreply.github.com>
2026-05-25 01:39:23 -0700

    Add functionality to print even and odd numbers

---
+++ even and odd numbers.py
@@ -0,0 +1,11 @@
+import numpy as np
+arr1=np.array([1, 2, 3, 4, 5])
+arr2=np.array([6, 7, 8, 9, 10])
+even_arr1=arr1[1:5:2]
+print("Even for array1",even_arr1)
+odd_arr1=arr1[0:5:2]
+print("Odd for array2",odd_arr1)
+even_arr2=arr2[0:5:2]
+print("Even for array2",even_arr2)
+odd_arr2=arr2[1:5:2]
+print("Odd for array2",odd_arr2)
