5d62b517289085a93baccca9d3a70f176f8c69d1
Sandeep K S <61325558+sandeepks011999@users.noreply.github.com>
2026-05-25 01:43:18 -0700

    Add basic NumPy operations in Numpy Maths.py

---
+++ Numpy Maths.py
@@ -0,0 +1,18 @@
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([8, 9, 10])
+
+arr3 = np.add(arr1, arr2)
+arr3
+
+arr4 = np.subtract(arr1, arr2)
+arr4
+
+arr5 = np.multiply(arr1, arr2)
+arr5
+
+
+arr6 = np.divide(arr1, arr2)
+arr6
+
+arr7 = np.dot(arr1, arr2)
+arr7
